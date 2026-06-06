8bd8190e9edfc3ef2146a21e7713ea3c48e6cb5d
klsteele64-tech <klsteele64@gmail.com>
2026-06-05 23:23:38 -0700

    Delete apps/burger-buds directory

--- apps/burger-buds/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  /** "./" for Capacitor; "/apps/burger-buds/" when built for steele.bz */
-  base: process.env.VITE_BASE ?? "./",
-  plugins: [react()],
-});
